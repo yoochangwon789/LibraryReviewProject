d148736b8c5f8cbff8520951b0ee0dc6c306ff7c
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-03 20:43:39 +0900

    Feat : Book 데이터 클래스 파라미터 추가

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/model/Book.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/model/Book.kt
@@ -1,5 +1,11 @@
 package com.yoochangwonspro.libraryreviewproject.model
 
-data class Book(
+import com.google.gson.annotations.SerializedName
+import java.io.FileDescriptor
 
+data class Book(
+    @SerializedName("itemId") val id: Long,
+    @SerializedName("title") val title: String,
+    @SerializedName("descriptor") val descriptor: String,
+    @SerializedName("coverSmallUrl") val coverSmallUrl: String
 )
