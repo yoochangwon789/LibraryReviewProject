ea12733217d86ce44b2721abca24c66fccf589cd
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-03 20:56:00 +0900

    Feat : Retrofit 초기 사용 셋팅 및 service create 작업까지 구현

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/MainActivity.kt
@@ -2,10 +2,22 @@ package com.yoochangwonspro.libraryreviewproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoochangwonspro.libraryreviewproject.api.BookService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://book.interpark.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val bookService = retrofit.create(BookService::class.java)
+
+
     }
 }
