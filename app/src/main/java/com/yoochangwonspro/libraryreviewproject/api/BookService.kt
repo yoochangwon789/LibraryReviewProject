bd90ac9871a7a92bd3fd7a6628e1f5e03d6caffa
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-03 20:26:42 +0900

    Feat : getBooksByName 함수 초기 셋팅

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/api/BookService.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/api/BookService.kt
@@ -1,4 +1,14 @@
 package com.yoochangwonspro.libraryreviewproject.api
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface BookService {
+
+    @GET("/api/bestSeller.api?output=json")
+    fun getBooksByName(
+        @Query("key") apiKey: String,
+        @Query("query") keyword: String
+    ) : Call<>
 }
