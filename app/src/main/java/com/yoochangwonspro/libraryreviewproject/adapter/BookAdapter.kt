c9e0d0e37fc9179ee0d27220434a725e3dc60623
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-04 13:00:27 +0900

    Feat : BookAdapter 클래스 RecyclerView Adapter 초기 설정 작업

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/BookAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/BookAdapter.kt
@@ -1,4 +1,11 @@
 package com.yoochangwonspro.libraryreviewproject.adapter
 
-class BookAdapter {
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class BookAdapter : ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) {
+
+    inner class BookItemViewHolder(private val binding: ItemBookBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    }
 }
