82ed28883a90d93a8384093966ba0e5f427b48e9
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-05 19:50:47 +0900

    Feat : onCreateViewHolder 함수의 ViewHolder 생성 작업

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/BookAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/BookAdapter.kt
@@ -1,5 +1,6 @@
 package com.yoochangwonspro.libraryreviewproject.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -11,13 +12,15 @@ class BookAdapter : ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) 
     inner class BookItemViewHolder(private val binding: ItemBookBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-            fun bind(bookModel: Book) {
-                binding.itemTitleTextView.text = bookModel.title
-            }
+        fun bind(bookModel: Book) {
+            binding.itemTitleTextView.text = bookModel.title
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookItemViewHolder {
-
+        return BookItemViewHolder(ItemBookBinding.inflate(LayoutInflater.from(parent.context),
+            parent,
+            false))
     }
 
     override fun onBindViewHolder(holder: BookItemViewHolder, position: Int) {
