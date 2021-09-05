322adfd843a710438141aa5007c03c6430349aa7
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-05 19:31:10 +0900

    Feat : BookAdapter 클래스의 ListAdapter 의 필요한 메서드 오버라이딩 작업

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/BookAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/BookAdapter.kt
@@ -1,11 +1,22 @@
 package com.yoochangwonspro.libraryreviewproject.adapter
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.yoochangwonspro.libraryreviewproject.databinding.ItemBookBinding
+import com.yoochangwonspro.libraryreviewproject.model.Book
 
 class BookAdapter : ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) {
 
     inner class BookItemViewHolder(private val binding: ItemBookBinding) :
         RecyclerView.ViewHolder(binding.root) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BookItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
 }
