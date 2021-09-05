add24c5d2fd0e6c4007333be20fd5155a37cae0e
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-05 20:07:20 +0900

    Feat : DiffUtil 초기 생성 작업 및 ItemCallBack 필수 함수 오버라이딩 작업

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/BookAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/BookAdapter.kt
@@ -2,6 +2,7 @@ package com.yoochangwonspro.libraryreviewproject.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.yoochangwonspro.libraryreviewproject.databinding.ItemBookBinding
@@ -25,5 +26,22 @@ class BookAdapter : ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) 
 
     override fun onBindViewHolder(holder: BookItemViewHolder, position: Int) {
         holder.bind(currentList[position])
+    }
+
+    // DiffUtil
+    // 리싸이클러뷰가 뷰의 position 이 변경이 되었을 때 새로운 값을 할당할지 말지 결정하는 기준이있다.
+    // 같은 아이템이 또 올라오게 되면 굳이 같은 것을 또 할당할 필요가 없게 된다.
+    // 그것을 결정이나 판단을 해주는 것이 DiffUtil 이다.
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<Book>() {
+            override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
+                TODO("Not yet implemented")
+            }
+        }
     }
 }
