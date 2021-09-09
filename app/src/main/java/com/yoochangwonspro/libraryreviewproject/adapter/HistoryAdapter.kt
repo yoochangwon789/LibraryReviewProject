e002b85edc31ad30e7351bd4c38ab7cc7d6c1076
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-10 02:28:48 +0900

    Feat : HistoryAdapter 클래스 ListAdapter 상속 및 diffUtil 초기 셋팅

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/HistoryAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/adapter/HistoryAdapter.kt
@@ -1,5 +1,40 @@
 package com.yoochangwonspro.libraryreviewproject.adapter
 
-class HistoryAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.yoochangwonspro.libraryreviewproject.databinding.ItemHistoryBinding
+import com.yoochangwonspro.libraryreviewproject.model.History
 
+
+class HistoryAdapter : ListAdapter<History, HistoryAdapter.HistoryItemViewHolder>(diffUtil) {
+
+    inner class HistoryItemViewHolder(private val binding: ItemHistoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(historyModel: History) {
+
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HistoryItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<History>() {
+            override fun areItemsTheSame(oldItem: History, newItem: History): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: History, newItem: History): Boolean {
+                return oldItem.uid == newItem.uid
+            }
+        }
+    }
 }
