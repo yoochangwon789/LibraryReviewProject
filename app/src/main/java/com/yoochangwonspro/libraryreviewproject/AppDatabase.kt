39e268205d05ac1496503cd2642004df92a21c4b
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-10 01:29:29 +0900

    Feat : AppDatabase RoomDatabase 클래스 상속 및 초기 셋업

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/AppDatabase.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/AppDatabase.kt
@@ -1,5 +1,9 @@
 package com.yoochangwonspro.libraryreviewproject
 
-abstract class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
 
+@Database(entities = [History::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun historyDao(): HistoryDao
 }
