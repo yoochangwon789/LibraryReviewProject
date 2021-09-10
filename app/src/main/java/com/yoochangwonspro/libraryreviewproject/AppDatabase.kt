8ec27883627b93f065ee9e33f8c7072c842985c5
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-11 02:15:21 +0900

    Feat : migration 생성 및 구현 작업

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/AppDatabase.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/AppDatabase.kt
@@ -4,21 +4,30 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.yoochangwonspro.libraryreviewproject.dao.HistoryDao
 import com.yoochangwonspro.libraryreviewproject.dao.ReviewDao
 import com.yoochangwonspro.libraryreviewproject.model.History
 import com.yoochangwonspro.libraryreviewproject.model.Review
 
-@Database(entities = [History::class, Review::class], version = 1)
-abstract class AppDatabase: RoomDatabase() {
+@Database(entities = [History::class, Review::class], version = 2)
+abstract class AppDatabase : RoomDatabase() {
     abstract fun historyDao(): HistoryDao
     abstract fun reviewDao(): ReviewDao
 }
 
 fun getAppDatabase(context: Context): AppDatabase {
+
+    val migration_1_2 = object : Migration(1, 2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("CREATE TABLE `REVIEW` (`id` INTEGER, `review` TEXT," + "PRIMARY KEY(`id`))")
+        }
+    }
+
     return Room.databaseBuilder(
         context,
         AppDatabase::class.java,
         "BookSearchDB"
-    ).build()
+    ).addMigrations(migration_1_2).build()
 }
