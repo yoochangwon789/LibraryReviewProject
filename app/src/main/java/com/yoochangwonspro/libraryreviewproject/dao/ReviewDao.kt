7d95c6d412298076bce71813532188a1920b1008
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-10 20:11:56 +0900

    Feat : ReviewDao saveReview 함수 추가

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/dao/ReviewDao.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/dao/ReviewDao.kt
@@ -1,6 +1,8 @@
 package com.yoochangwonspro.libraryreviewproject.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.yoochangwonspro.libraryreviewproject.model.Review
 
@@ -9,4 +11,7 @@ interface ReviewDao {
 
     @Query("SELECT * FROM review WHERE id == :id")
     fun getOnReview(id: Int): Review
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveReview(review: Review)
 }
