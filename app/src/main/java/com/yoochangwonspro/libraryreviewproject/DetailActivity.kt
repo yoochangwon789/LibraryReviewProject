b8d41e1c51dafb4ec69c683788da96310f262183
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-10 18:23:16 +0900

    Feat : Room 데이터 베이스 가져오는 초기 셋업

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/DetailActivity.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/DetailActivity.kt
@@ -2,15 +2,23 @@ package com.yoochangwonspro.libraryreviewproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
 import com.yoochangwonspro.libraryreviewproject.databinding.ActivityDetailBinding
 
-class DetailActivity: AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
+    private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "BookSearchDB"
+        ).build()
     }
 }
