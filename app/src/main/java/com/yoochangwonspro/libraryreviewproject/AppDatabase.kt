f19851df73cf007abb61100672c67994e41a8f27
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-10 20:12:51 +0900

    Feat : AppDatabase reviewDao 인터페이스 추가

--- app/src/main/java/com/yoochangwonspro/libraryreviewproject/AppDatabase.kt
+++ app/src/main/java/com/yoochangwonspro/libraryreviewproject/AppDatabase.kt
@@ -3,9 +3,12 @@ package com.yoochangwonspro.libraryreviewproject
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.yoochangwonspro.libraryreviewproject.dao.HistoryDao
+import com.yoochangwonspro.libraryreviewproject.dao.ReviewDao
 import com.yoochangwonspro.libraryreviewproject.model.History
+import com.yoochangwonspro.libraryreviewproject.model.Review
 
-@Database(entities = [History::class], version = 1)
+@Database(entities = [History::class, Review::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun historyDao(): HistoryDao
+    abstract fun reviewDao(): ReviewDao
 }
