bb802fb7eb0a1324473133b4e53eb1a45e73a68a
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-12-25 15:18:44 +0900

    Feat : activity_main.xml 레이아웃 디자인 구성 작업

--- app/app2/src/main/java/com/yoochangwonspro/app2/MainActivity.kt
+++ app/app2/src/main/java/com/yoochangwonspro/app2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.yoochangwonspro.app2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoochangwonspro.app2.response.MyKey
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
