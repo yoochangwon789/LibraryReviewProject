676ff061c0b736d94395cf0da6e03c700ddb056d
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-12-26 16:38:20 +0900

    Feat : MainActivity view binding 적용

--- app/app2/src/main/java/com/yoochangwonspro/app2/MainActivity.kt
+++ app/app2/src/main/java/com/yoochangwonspro/app2/MainActivity.kt
@@ -2,11 +2,16 @@ package com.yoochangwonspro.app2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yoochangwonspro.app2.databinding.ActivityMainBinding
 import com.yoochangwonspro.app2.response.MyKey
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
