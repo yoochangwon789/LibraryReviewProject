ca11bf1b571f23ebf53a299b2932098f7a752e1c
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-12-27 13:04:10 +0900

    Feat : INTERPARK_URL 수정

--- app/app2/src/main/java/com/yoochangwonspro/app2/response/Url.kt
+++ app/app2/src/main/java/com/yoochangwonspro/app2/response/Url.kt
@@ -2,5 +2,5 @@ package com.yoochangwonspro.app2.response
 
 object Url {
 
-    const val INTERPARK_URL = "http://book.interpark.com/api/search.api/"
+    const val INTERPARK_URL = "https://book.interpark.com/api/"
 }
