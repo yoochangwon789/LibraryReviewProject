020631295364e0bb36a4cd68df6c5a260cf74b69
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-12-27 13:35:02 +0900

    Feat : getBestSeller GET 서비스 함수 추가

--- app/app2/src/main/java/com/yoochangwonspro/app2/utillity/BookService.kt
+++ app/app2/src/main/java/com/yoochangwonspro/app2/utillity/BookService.kt
@@ -1,9 +1,12 @@
 package com.yoochangwonspro.app2.utillity
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface BookService {
 
-    @GET("bestSeller.api?output=json")
-    fun getBestSeller()
+    @GET("bestSeller.api?output=json/categoryId=100")
+    fun getBestSeller(
+        @Query("key") key: String,
+    )
 }
