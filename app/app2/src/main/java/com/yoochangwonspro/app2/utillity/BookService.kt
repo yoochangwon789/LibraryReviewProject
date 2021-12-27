679062b2db4a9f4f4297ea65ac3339c77aa7b7c4
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-12-27 13:36:54 +0900

    Feat : getBestSeller return type Call 함수 추가

--- app/app2/src/main/java/com/yoochangwonspro/app2/utillity/BookService.kt
+++ app/app2/src/main/java/com/yoochangwonspro/app2/utillity/BookService.kt
@@ -1,5 +1,7 @@
 package com.yoochangwonspro.app2.utillity
 
+import com.yoochangwonspro.app2.dto.BestSellerDto
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -7,6 +9,6 @@ interface BookService {
 
     @GET("bestSeller.api?output=json/categoryId=100")
     fun getBestSeller(
-        @Query("key") key: String,
-    )
+        @Query("key") key: String
+    ): Call<BestSellerDto>
 }
