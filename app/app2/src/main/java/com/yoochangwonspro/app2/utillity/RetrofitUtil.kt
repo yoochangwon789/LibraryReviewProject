90b6843093e519c0d7957f162f96b176958b5389
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-12-27 13:39:47 +0900

    Feat : bookService 싱글턴 인스턴스 추가

--- app/app2/src/main/java/com/yoochangwonspro/app2/utillity/RetrofitUtil.kt
+++ app/app2/src/main/java/com/yoochangwonspro/app2/utillity/RetrofitUtil.kt
@@ -1,10 +1,15 @@
 package com.yoochangwonspro.app2.utillity
 
+import com.yoochangwonspro.app2.response.MyKey
 import com.yoochangwonspro.app2.response.Url
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitUtil {
+
+    val bookService: BookService by lazy {
+        getRetrofit().create(BookService::class.java)
+    }
 
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
