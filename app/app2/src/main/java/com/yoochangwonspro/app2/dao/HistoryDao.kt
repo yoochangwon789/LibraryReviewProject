444380459970a1a390b641dc314223e74d1f5b4c
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-12-28 18:12:52 +0900

    Add : HistoryDao 추가 get, insert, delete 쿼리 추가

---
+++ app/app2/src/main/java/com/yoochangwonspro/app2/dao/HistoryDao.kt
@@ -0,0 +1,20 @@
+package com.yoochangwonspro.app2.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.yoochangwonspro.app2.model.History
+
+@Dao
+interface HistoryDao {
+
+    @Query("SELECT * FROM history")
+    suspend fun getAll(): List<History>
+
+    @Insert
+    suspend fun insertHistory(history: History)
+
+    @Query("SELECT * FROM history WHERE history_name == :historyName")
+    suspend fun deleteHistory(historyName: String)
+}
