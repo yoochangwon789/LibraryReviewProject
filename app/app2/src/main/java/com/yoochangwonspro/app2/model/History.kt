a97ec6363126ea95ce204c2ec659156cbffa0536
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-12-28 18:08:54 +0900

    Add : History Entity model 추가

---
+++ app/app2/src/main/java/com/yoochangwonspro/app2/model/History.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.app2.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class History(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "history_name") val historyName: String?
+)
